21f222fc13d76d14c19f03a467ee2bdd01108221
hanene <nebi.hanene@gmail.com>
2018-08-16 13:32:49 +0200

    first

---
+++ src/main/java/web/com/ServeurGestionStockProdApplication.java
@@ -0,0 +1,12 @@
+package web.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProdApplication.class, args);
+	}
+}
