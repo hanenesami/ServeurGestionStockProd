90d712ef943ff180707f0c14b4bef407160a4701
hanene <nebi.hanene@gmail.com>
2018-08-25 17:23:33 +0200

    crud produits

---
+++ src/main/java/web/com/service/ProduitMockServiceImpl.java
@@ -0,0 +1,47 @@
+package web.com.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import web.com.entity.Produit;
+
+@Service
+public class ProduitMockServiceImpl implements IProduitService {
+
+	public ProduitMockServiceImpl(){
+		produits=new ArrayList<>();
+		produits.add(new Produit("Livre", 50, 20));
+		produits.add(new Produit("Cahier", 200, 20));
+		produits.add(new Produit("Stylo", 500, 20));
+		
+	}
+	private List<Produit> produits;
+	@Override
+	public List<Produit> getProduits() {
+		// TODO Auto-generated method stub
+		return produits;
+	}
+
+	@Override
+	public void addProduit(Produit produit) {
+		produits.add(produit);
+		
+	}
+
+	@Override
+	public void updateProduit(Produit produit) {
+		produits.remove(produit);
+		produits.add(produit);
+		
+	}
+
+	@Override
+	public void deleteProduit(String ref) {
+		Produit produit=new Produit();
+		produits.remove(produit);
+		
+	}
+
+}
